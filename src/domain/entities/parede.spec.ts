48a73c35ce9dd3e373a9fe92adf7cfcd318abe97
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-04-24 03:26:27 -0300

    modifica entidade parede e adiciona tests

--- src/domain/entities/parede.spec.ts
+++ src/domain/entities/parede.spec.ts
@@ -1,20 +1,234 @@
+import { ParedeEnum } from '../enums/parede-enum'
+import { IParede } from '../interfaces/parede'
 import { Parede } from './parede'
 
 describe('Parede', () => {
-  describe('calcularArea', () => {
-    it('deve calcular a área corretamente quando não há portas ou janelas', () => {
-      const parede = new Parede(3, 4, 0, 0)
-      expect(parede.calcularArea()).toBe(12)
+  describe('calculaAreaParede', () => {
+    it('deve calcular a área correta da parede', () => {
+      expect(Parede.calculaAreaParede(2, 4)).toBe(8)
+      expect(Parede.calculaAreaParede(3, 5)).toBe(15)
+      expect(Parede.calculaAreaParede(4, 4)).toBe(16)
     })
 
-    it('deve calcular a área corretamente quando há portas e janelas', () => {
-      const parede = new Parede(3, 4, 1, 1)
-      expect(parede.calcularArea()).toBeCloseTo(8.08, 2)
+    it('deve lançar um erro se a área total da parede for menor que 1m²', () => {
+      expect(() => Parede.calculaAreaParede(0.4, 2)).toThrowError(
+        'A área total da parede deve estar entre 1 e 50m², área atual (0.8m²).'
+      )
     })
 
-    it('deve calcular a área corretamente quando há várias portas e janelas', () => {
-      const parede = new Parede(3, 4, 2, 3)
-      expect(parede.calcularArea()).toBeCloseTo(2.64, 2)
+    it('deve lançar um erro se a área total da parede for maior que 50m²', () => {
+      expect(() => Parede.calculaAreaParede(10, 10)).toThrowError(
+        'A área total da parede deve estar entre 1 e 50m², área atual (100m²).'
+      )
+    })
+  })
+
+  describe('calcularAreaTotalParedes', () => {
+    it('deve calcular corretamente a área total das paredes', () => {
+      const paredes: IParede[] = [
+        { altura: 3, largura: 5 },
+        { altura: 3, largura: 4 },
+        { altura: 3, largura: 6 },
+        { altura: 3, largura: 3 }
+      ]
+
+      const areaTotalEsperada = 54
+
+      expect(Parede.calculaAreaTotalParedes(paredes)).toEqual(areaTotalEsperada)
+    })
+  })
+
+  describe('calculaAreaJanela', () => {
+    it('deve calcular a área correta da janela', () => {
+      const quantidadeJanela = 3
+      const areaJanela = ParedeEnum.ALTURA_JANELA * ParedeEnum.LARGURA_JANELA
+      const areaTotalJanela = quantidadeJanela * areaJanela
+
+      expect(Parede.calculaAreaJanela(quantidadeJanela)).toEqual(areaTotalJanela)
+    })
+  })
+
+  describe('calculaAreaTotalJanelas', () => {
+    it('deve retornar 0 quando não houver janelas em nenhuma parede', () => {
+      const paredes: IParede[] = [
+        { altura: 3, largura: 5, quantidadeJanela: 0 },
+        { altura: 3, largura: 4, quantidadeJanela: 0 },
+        { altura: 3, largura: 6, quantidadeJanela: 0 },
+        { altura: 3, largura: 3, quantidadeJanela: 0 }
+      ]
+
+      const areaTotalEsperada = ParedeEnum.ALTURA_JANELA * ParedeEnum.LARGURA_JANELA * 0
+
+      expect(Parede.calcularAreaTotalJanelas(paredes)).toBe(areaTotalEsperada)
+    })
+
+    it('deve calcular corretamente a área total de janelas quando houver janelas em todas as paredes', () => {
+      const paredes: IParede[] = [
+        { altura: 3, largura: 5, quantidadeJanela: 2 },
+        { altura: 3, largura: 4, quantidadeJanela: 2 },
+        { altura: 3, largura: 6, quantidadeJanela: 2 },
+        { altura: 3, largura: 3, quantidadeJanela: 2 }
+      ]
+
+      const areaTotalEsperada = ParedeEnum.ALTURA_JANELA * ParedeEnum.LARGURA_JANELA * 8
+
+      expect(Parede.calcularAreaTotalJanelas(paredes)).toEqual(areaTotalEsperada)
+    })
+
+    it('deve calcular corretamente a área total de janelas quando houver janelas em algumas paredes', () => {
+      const paredes: IParede[] = [
+        { altura: 3, largura: 5, quantidadeJanela: 2 },
+        { altura: 3, largura: 4, quantidadeJanela: 0 },
+        { altura: 3, largura: 6, quantidadeJanela: 1 },
+        { altura: 3, largura: 3, quantidadeJanela: 3 }
+      ]
+
+      const areaTotalEsperada = ParedeEnum.ALTURA_JANELA * ParedeEnum.LARGURA_JANELA * 6
+
+      expect(Parede.calcularAreaTotalJanelas(paredes)).toBeCloseTo(areaTotalEsperada)
+    })
+  })
+
+  describe('calculaAreaPorta', () => {
+    it('deve calcular a área correta da porta', () => {
+      const quantidadePorta = 3
+      const areaPorta = ParedeEnum.ALTURA_PORTA * ParedeEnum.LARGURA_PORTA
+      const areaTotalPorta = quantidadePorta * areaPorta
+
+      expect(Parede.calculaAreaPorta(quantidadePorta)).toEqual(areaTotalPorta)
+    })
+  })
+
+  describe('calculaAreaTotalPortas', () => {
+    it('deve retornar 0 quando não houver portas em nenhuma parede', () => {
+      const paredes: IParede[] = [
+        { altura: 3, largura: 5, quantidadePorta: 0 },
+        { altura: 3, largura: 4, quantidadePorta: 0 },
+        { altura: 3, largura: 6, quantidadePorta: 0 },
+        { altura: 3, largura: 3, quantidadePorta: 0 }
+      ]
+
+      const areaTotalEsperada = ParedeEnum.ALTURA_PORTA * ParedeEnum.LARGURA_PORTA * 0
+
+      expect(Parede.calcularAreaTotalPortas(paredes)).toBe(areaTotalEsperada)
+    })
+
+    it('deve calcular corretamente a área total de portas quando houver portas em todas as paredes', () => {
+      const paredes: IParede[] = [
+        { altura: 3, largura: 5, quantidadePorta: 2 },
+        { altura: 3, largura: 4, quantidadePorta: 2 },
+        { altura: 3, largura: 6, quantidadePorta: 2 },
+        { altura: 3, largura: 3, quantidadePorta: 2 }
+      ]
+
+      const areaTotalEsperada = ParedeEnum.ALTURA_PORTA * ParedeEnum.LARGURA_PORTA * 8
+
+      expect(Parede.calcularAreaTotalPortas(paredes)).toEqual(areaTotalEsperada)
+    })
+
+    it('deve calcular corretamente a área total de portas quando houver portas em algumas paredes', () => {
+      const paredes: IParede[] = [
+        { altura: 3, largura: 5, quantidadePorta: 2 },
+        { altura: 3, largura: 4, quantidadePorta: 0 },
+        { altura: 3, largura: 6, quantidadePorta: 1 },
+        { altura: 3, largura: 3, quantidadePorta: 3 }
+      ]
+
+      const areaTotalEsperada = ParedeEnum.ALTURA_PORTA * ParedeEnum.LARGURA_PORTA * 6
+
+      expect(Parede.calcularAreaTotalPortas(paredes)).toBeCloseTo(areaTotalEsperada)
+    })
+  })
+
+  describe('verificaAreaTotalJanelaComPorta', () => {
+    it('deve retornar erro quando a área total das janelas e portas for superior a 50% da área total da parede', () => {
+      const areaTotalJanela = 20
+      const areaTotalPorta = 15
+      const areaTotalParede = 50
+
+      expect(() => Parede.verificaAreaTotalJanelaComPorta(areaTotalJanela, areaTotalPorta, areaTotalParede))
+        .toThrow('A área total das portas e janelas não pode ser superior a 50% da área total da parede')
+    })
+
+    it('não deve retornar erro quando a área total das janelas e portas for igual a 50% da área total da parede', () => {
+      const areaTotalJanela = 12.5
+      const areaTotalPorta = 12.5
+      const areaTotalParede = 50
+
+      expect(() => Parede.verificaAreaTotalJanelaComPorta(areaTotalJanela, areaTotalPorta, areaTotalParede))
+        .not.toThrow()
+    })
+
+    it('não deve retornar erro quando a área total das janelas e portas for inferior a 50% da área total da parede', () => {
+      const areaTotalJanela = 10
+      const areaTotalPorta = 5
+      const areaTotalParede = 50
+
+      expect(() => Parede.verificaAreaTotalJanelaComPorta(areaTotalJanela, areaTotalPorta, areaTotalParede))
+        .not.toThrow()
+    })
+  })
+
+  describe('verificaAlturaMinimaParedeComPorta', () => {
+    it('não deve lançar erro se a altura da parede for maior ou igual à altura mínima com porta', () => {
+      const alturaParede = ParedeEnum.ALTURA_PORTA + ParedeEnum.DIFERENCA_MINIMA_ALTURA
+      expect(() => Parede.verificaAlturaMinimaParedeComPorta(alturaParede)).not.toThrow()
+    })
+
+    it('deve lançar erro se a altura da parede for menor que a altura mínima com porta', () => {
+      const alturaParede = ParedeEnum.ALTURA_PORTA + ParedeEnum.DIFERENCA_MINIMA_ALTURA - 1
+      expect(() => Parede.verificaAlturaMinimaParedeComPorta(alturaParede))
+        .toThrow(Error('As paredes que possuem portas devem ser no mínimo 30cm maiores que a altura da porta'))
+    })
+  })
+
+  describe('calculaQuantidadeTinta', () => {
+    it('deve retornar a quantidade de tinta necessária para pintar a área total da parede', () => {
+      const area = 18
+      const resultadoEsperado = {
+        area,
+        litrosNecessarios: area / 5,
+        latasNecessarias: [
+          { tamanho: 18, quantidade: 0 },
+          { tamanho: 3.6, quantidade: 1 },
+          { tamanho: 2.5, quantidade: 0 },
+          { tamanho: 0.5, quantidade: 0 },
+        ],
+      }
+      const resultado = Parede.calculaQuantidadeTinta(area)
+      expect(resultado).toEqual(resultadoEsperado)
+    })
+
+    it('deve retornar a quantidade de tinta necessária para pintar uma parede com margem de erro', () => {
+      const area = 23
+      const resultadoEsperado = {
+        area,
+        litrosNecessarios: area / 5,
+        latasNecessarias: [
+          { tamanho: 18, quantidade: 0 },
+          { tamanho: 3.6, quantidade: 1 },
+          { tamanho: 2.5, quantidade: 0 },
+          { tamanho: 0.5, quantidade: 2 },
+        ],
+      }
+      const resultado = Parede.calculaQuantidadeTinta(area)
+      expect(resultado).toEqual(resultadoEsperado)
+    })
+
+    it('deve retornar a quantidade de tinta necessária para pintar uma área grande', () => {
+      const area = 50
+      const resultadoEsperado = {
+        area,
+        litrosNecessarios: area / 5,
+        latasNecessarias: [
+          { tamanho: 18, quantidade: 0 },
+          { tamanho: 3.6, quantidade: 2 },
+          { tamanho: 2.5, quantidade: 1 },
+          { tamanho: 0.5, quantidade: 1 },
+        ],
+      }
+      const resultado = Parede.calculaQuantidadeTinta(area)
+      expect(resultado).toEqual(resultadoEsperado)
     })
   })
 })
--- src/domain/entities/parede.ts
+++ src/domain/entities/parede.ts
@@ -1,26 +1,122 @@
+import { LataTintaEnum, LitrosPorMetroQuadradoEnum } from '../enums/lata-tinta-enum'
 import { ParedeEnum } from '../enums/parede-enum'
+import { ICalculoQuantidadeTinta, IParede } from '../interfaces/parede'
 
 export class Parede {
-  altura: number
-  largura: number
-  area: number
-  quantidadeJanela: number
-  quantidadePorta: number
+  static calculaAreaParede(altura: number, largura: number): number {
+    const areaTotal = altura * largura
 
-  constructor(altura: number, largura: number, quantidadeJanela: number, quantidadePorta: number) {
-    this.altura = altura
-    this.largura = largura
-    this.quantidadeJanela = quantidadeJanela
-    this.quantidadePorta = quantidadePorta
-    this.area = this.calcularArea()
+    if (areaTotal < ParedeEnum.AREA_MINIMA || areaTotal > ParedeEnum.AREA_MAXIMA) {
+      throw new Error(`A área total da parede deve estar entre 1 e 50m², área atual (${areaTotal}m²).`)
+    }
+
+    return areaTotal
   }
 
-  calcularArea(): number {
-    const areaTotal = this.altura * this.largura
-    const areaJanelas = this.quantidadeJanela * ParedeEnum.AREA_JANELA
-    const areaPortas = this.quantidadePorta * ParedeEnum.AREA_PORTA
+  static calculaAreaTotalParedes(paredes: IParede[]) {
+    const areas = []
 
-    return areaTotal - areaJanelas - areaPortas
+    for (const parede of paredes) {
+      const area = Parede.calculaAreaParede(parede.altura, parede.largura)
+      areas.push(area)
+    }
+
+    const areaTotal = areas.reduce((total, area) => total + area, 0)
+    return areaTotal
+  }
+
+  static calculaAreaJanela(quantidadeJanela: number): number {
+    const areaJanela = ParedeEnum.ALTURA_JANELA * ParedeEnum.LARGURA_JANELA
+    return quantidadeJanela * areaJanela
+  }
+
+  static calcularAreaTotalJanelas(paredes: IParede[]) {
+    const areas = []
+
+    for (const parede of paredes) {
+      if (parede.quantidadeJanela) {
+        const area = Parede.calculaAreaJanela(parede.quantidadeJanela)
+        areas.push(area)
+      }
+    }
+
+    const areaTotal = areas.reduce((total, area) => total + area, 0)
+    return areaTotal
+  }
+
+  static calculaAreaPorta(quantidadePorta: number): number {
+    const areaPorta = ParedeEnum.ALTURA_PORTA * ParedeEnum.LARGURA_PORTA
+    return quantidadePorta * areaPorta
+  }
+
+  static calcularAreaTotalPortas(paredes: IParede[]) {
+    const areas = []
+
+    for (const parede of paredes) {
+      if (parede.quantidadePorta) {
+        const area = Parede.calculaAreaPorta(parede.quantidadePorta)
+        areas.push(area)
+      }
+    }
+
+    const areaTotal = areas.reduce((total, area) => total + area, 0)
+    return areaTotal
+  }
+
+  static verificaAreaTotalJanelaComPorta(areaTotalJenela: number, areaTotalPorta: number, areaTotalParede: number): void {
+    const areaTotalJanelaComPorta = areaTotalJenela + areaTotalPorta
+    const areaMaximaParede = areaTotalParede / 2
+
+    if (areaTotalJanelaComPorta > areaMaximaParede) {
+      throw new Error('A área total das portas e janelas não pode ser superior a 50% da área total da parede')
+    }
+  }
+
+  static verificaAlturaMinimaParedeComPorta(alturaParede: number): void {
+    const alturaMinimaParede = ParedeEnum.ALTURA_PORTA + ParedeEnum.DIFERENCA_MINIMA_ALTURA
+
+    if (alturaParede < alturaMinimaParede) {
+      throw new Error('As paredes que possuem portas devem ser no mínimo 30cm maiores que a altura da porta')
+    }
+  }
+
+  static calculaQuantidadeTinta(area: number): ICalculoQuantidadeTinta {
+    const TAMANHOS_LATA = Object.keys(LataTintaEnum)
+      .map((x) => Number(x))
+      .filter((x) => !isNaN(x))
+
+    const litrosTotaisNecessarios = area / LitrosPorMetroQuadradoEnum.LITROS_POR_METRO_QUADRADO
+
+    let litrosNecessarios = litrosTotaisNecessarios
+    const latasNecessarias: Array<{ tamanho: number, quantidade: number }> = []
+
+    for (const tamanho of TAMANHOS_LATA) {
+      const quantidade = Math.floor(litrosNecessarios / tamanho)
+      for (let i = 0; i < quantidade; i++) {
+        latasNecessarias.push({ tamanho, quantidade: 1 })
+      }
+      litrosNecessarios %= tamanho
+    }
+
+    if (litrosNecessarios > 0 &&
+      litrosNecessarios <= TAMANHOS_LATA[TAMANHOS_LATA.length - 1] +
+      LitrosPorMetroQuadradoEnum.MARGEM_ERRO
+    ) {
+      latasNecessarias.push({
+        tamanho: TAMANHOS_LATA[TAMANHOS_LATA.length - 1],
+        quantidade: 1,
+      })
+    }
+
+    return {
+      area,
+      litrosNecessarios: litrosTotaisNecessarios,
+      latasNecessarias: TAMANHOS_LATA.map((tamanho) => ({
+        tamanho,
+        quantidade: latasNecessarias.filter((lata) => lata.tamanho === tamanho)
+          .length,
+      })),
+    }
   }
 }
 
