76497c34c2c0d935ed0622a0600899bf3e0ae06e
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-04-24 03:24:54 -0300

    adiciona lata tinta e parede enum

---
+++ src/domain/enums/lata-tinta-enum.ts
@@ -0,0 +1,13 @@
+export enum LataTintaEnum {
+  LATA_18 = 18,
+  LATA_3_6 = 3.6,
+  LATA_2_5 = 2.5,
+  LATA_0_5 = 0.5,
+}
+
+export enum LitrosPorMetroQuadradoEnum {
+  LITROS_POR_METRO_QUADRADO = 5,
+  MARGEM_ERRO = 0.5
+}
+
+
