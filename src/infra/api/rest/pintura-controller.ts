56e67399fa222eb9894e6551f6dcc151727405db
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-04-24 03:31:53 -0300

    modifica o controller adicionando usecase calculaQuantidadeTinta, logger e try/catch

--- src/infra/api/rest/pintura-controller.ts
+++ src/infra/api/rest/pintura-controller.ts
@@ -1,8 +1,24 @@
 import { Request, Response } from 'express'
+import { CalculaQuantidadeTintaUseCase } from '../../../domain/usecases/calculaQuantidadeTinta-usecase'
+import logger from '../../helper/logger'
 
 export class PinturaController {
-  calculaQuantidadeTinta(request: Request, response: Response): Response {
-    return response.json({ message: 'Hello World' })
+  constructor(private readonly calculaQuantidadeTinta: CalculaQuantidadeTintaUseCase) { }
+
+  calculaTinta(request: Request, response: Response): Response {
+    try {
+      if (!Array.isArray(request.body) || !request.body.length) {
+        return response.status(400).json({ error: 'Invalid request body' })
+      }
+
+      const quantidaTinta = this.calculaQuantidadeTinta.calculaQuantidade(request.body)
+
+      logger.info({ req: request, res: response })
+
+      return response.json(quantidaTinta)
+    } catch (error: any) {
+      logger.warn(error)
+      return response.status(400).json({ message: error.message })
+    }
   }
 }
-
